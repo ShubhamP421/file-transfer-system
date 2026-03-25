1a84a64b0b287cd271521e997d45e95d334cfde2
ShubhamP421 <shubhamprajapatitemporary@gmail.com>
2026-03-26 00:04:59 +0530

    Backend changes

--- server.js
+++ server.js
@@ -104,7 +104,9 @@ app.get("/file/:code", (req, res) => {
 /* =========================
    SERVER START
 ========================= */
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`🔥 Server running on http://localhost:${PORT}`);
 });
